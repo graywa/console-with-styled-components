5a4a2ab118f22d6ce0fb5aa50574fa3f35501969
graywa <74241140+graywa@users.noreply.github.com>
2022-01-25 12:55:42 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.css'
+import styled from 'styled-components'
+
+const AppWrapper = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  padding: 2rem;
+  background: #2c2d30;
+  color: white;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppWrapper>
+      <div className='App'>
+        work
+      </div>
+    </AppWrapper>
+    
+  ) 
 }
 
-export default App;
+export default App
